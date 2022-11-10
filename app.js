1a7538b75de3bb90abb9b6d81aeb026466aa0953
blake S <design.harls@icloud.com>
2022-11-10 11:19:54 +0100

    declaracion de variables y funcion preplevadura

---
+++ app.js
@@ -0,0 +1,31 @@
+ /*declaracion de variables*/
+ const harina="harina de centeno";
+ const agua ="agua";
+ const aguaTemplada="agua templada";
+ const aceite="aceite";
+ const levandura="levadura";
+ const azucar="azucar";
+ const sal="sal";
+ const pepperoni="pepperoni";
+ const SalsaTomate="SalsaTomate";
+ const Chapinones="chapinones";
+ const aceitunas="aceitunas";
+ const mozzarella="mozzarella";
+
+let cantidadSal="1 cucharita de postre";
+let cantidadAzucar="1 cucharita de postre";
+let cantidadHarina="400 gramos";
+let cantidadAceituna="poner al gusto";
+let cantidadAgua="100 mililitros";
+let cantidadTemplada="100 mililitros";
+let cantidadLevadura="16 gramos";
+
+
+function prepararLevandura()
+{
+/*lo que muestra por pantalla*/
+console.log("Mezclar el  " +cantidadAgua+" de "+aguaTemplada+", el "+azucar);
+}
+prepararLevandura();
+
+
